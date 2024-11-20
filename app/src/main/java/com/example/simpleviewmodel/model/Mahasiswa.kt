ead93de883c41cce46ea3eab17927ea74808b280
adityapermono8 <adityapermono8@gmail.com>
2024-11-20 20:37:20 +0700

    Menambahkan data class Mahasiswa

---
+++ app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.simpleviewmodel.model
+
+data class Mahasiswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = "",
+    val email: String = "",
+    val nohp: String = "",
+)
