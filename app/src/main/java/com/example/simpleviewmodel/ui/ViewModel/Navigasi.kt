f74abb35ea2bb392ed69b1139ace76e85ebba01d
adityapermono8 <adityapermono8@gmail.com>
2024-11-20 23:35:05 +0700

    Membuat Fungsi Navigasi

---
+++ app/src/main/java/com/example/simpleviewmodel/ui/ViewModel/Navigasi.kt
@@ -0,0 +1,62 @@
+package com.example.simpleviewmodel.ui.ViewModel
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.simpleviewmodel.model.ListGender
+import com.example.simpleviewmodel.ui.view.DetailMahasiswaView
+import com.example.simpleviewmodel.ui.view.FormMahasiswaView
+
+
+enum class Halaman{
+    Form,
+    Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ) {
+            composable(route = Halaman.Form.name) {
+                val konteks = LocalContext.current
+                FormMahasiswaView(
+
+                    //Di bawah ini merupakan dari parameter halaman FormulirView
+                    listGender = ListGender.listGender.map { id -> konteks.getString(id)
+                    },
+                    onSubmitClick = {
+                        viewModel.savedDataMhs(it)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
+            composable(route = Halaman.Data.name) {
+                DetailMahasiswaView(
+                    dataMhs = uiState,
+                    modifier = Modifier,
+                    onClickButton = {
+                        navHost.popBackStack()
+                    }
+                )
+            }
+        }
+    }
+}
